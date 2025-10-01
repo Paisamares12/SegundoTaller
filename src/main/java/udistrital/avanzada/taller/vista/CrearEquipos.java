8b464196a44a1e48679db7f208b64969f316f625
Paula <paisamares12@gmail.com>
2025-09-30 20:33:11 -0500

    CrearEquipos algo mas definido

--- src/main/java/udistrital/avanzada/taller/vista/CrearEquipos.java
+++ src/main/java/udistrital/avanzada/taller/vista/CrearEquipos.java
@@ -74,17 +74,28 @@ public class CrearEquipos extends javax.swing.JFrame {
         lNumeroJugador = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         botonCrearJugador = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        botonPrevio = new javax.swing.JButton();
+        botonSiguiente = new javax.swing.JButton();
+        PanelImagenes = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         bg.setBackground(new java.awt.Color(15, 50, 17));
 
         jLabel1.setFont(new java.awt.Font("Monospaced", 1, 36)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel1.setText("Equipo");
 
         lTipoEquipo.setFont(new java.awt.Font("Monospaced", 1, 36)); // NOI18N
+        lTipoEquipo.setForeground(new java.awt.Color(255, 255, 255));
         lTipoEquipo.setText("A");
 
         jLabel3.setFont(new java.awt.Font("OCR A Extended", 1, 50)); // NOI18N
@@ -94,12 +105,58 @@ public class CrearEquipos extends javax.swing.JFrame {
         jLabel3.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
 
         lNumeroJugador.setFont(new java.awt.Font("Monospaced", 1, 24)); // NOI18N
+        lNumeroJugador.setForeground(new java.awt.Color(255, 255, 255));
         lNumeroJugador.setText("1");
 
         jLabel4.setFont(new java.awt.Font("Monospaced", 1, 24)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setText("Jugador");
 
-        botonCrearJugador.setText("jButton1");
+        botonCrearJugador.setBackground(new java.awt.Color(132, 72, 62));
+        botonCrearJugador.setFont(new java.awt.Font("Monospaced", 1, 18)); // NOI18N
+        botonCrearJugador.setForeground(new java.awt.Color(0, 0, 0));
+        botonCrearJugador.setText("Crear Jugador");
+        botonCrearJugador.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, null, new java.awt.Color(0, 0, 0), null, null));
+
+        jLabel2.setFont(new java.awt.Font("Monospaced", 1, 18)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Nombre:");
+
+        jLabel5.setFont(new java.awt.Font("Monospaced", 1, 18)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("Foto:");
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        botonPrevio.setBackground(new java.awt.Color(132, 72, 62));
+        botonPrevio.setFont(new java.awt.Font("Monospaced", 1, 12)); // NOI18N
+        botonPrevio.setForeground(new java.awt.Color(255, 255, 255));
+        botonPrevio.setText("<");
+        botonPrevio.setBorder(null);
+        jPanel2.add(botonPrevio, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 23, 140));
+
+        botonSiguiente.setBackground(new java.awt.Color(132, 72, 62));
+        botonSiguiente.setFont(new java.awt.Font("Monospaced", 1, 12)); // NOI18N
+        botonSiguiente.setForeground(new java.awt.Color(255, 255, 255));
+        botonSiguiente.setText(">");
+        botonSiguiente.setBorder(null);
+        jPanel2.add(botonSiguiente, new org.netbeans.lib.awtextra.AbsoluteConstraints(147, 0, 23, 140));
+
+        PanelImagenes.setBackground(new java.awt.Color(153, 0, 0));
+
+        javax.swing.GroupLayout PanelImagenesLayout = new javax.swing.GroupLayout(PanelImagenes);
+        PanelImagenes.setLayout(PanelImagenesLayout);
+        PanelImagenesLayout.setHorizontalGroup(
+            PanelImagenesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 130, Short.MAX_VALUE)
+        );
+        PanelImagenesLayout.setVerticalGroup(
+            PanelImagenesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 140, Short.MAX_VALUE)
+        );
+
+        jPanel2.add(PanelImagenes, new org.netbeans.lib.awtextra.AbsoluteConstraints(23, 0, 130, 140));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -109,8 +166,18 @@ public class CrearEquipos extends javax.swing.JFrame {
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
+            .addGap(0, 128, Short.MAX_VALUE)
         );
+
+        jTextField1.setFont(new java.awt.Font("Monospaced", 1, 14)); // NOI18N
+        jTextField1.setText("Ingrese su Apodo");
+
+        jLabel6.setFont(new java.awt.Font("Monospaced", 1, 18)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setText("Apodo:");
+
+        jTextField2.setFont(new java.awt.Font("Monospaced", 1, 14)); // NOI18N
+        jTextField2.setText("Ingrese su nombre");
 
         javax.swing.GroupLayout bgLayout = new javax.swing.GroupLayout(bg);
         bg.setLayout(bgLayout);
@@ -128,8 +195,24 @@ public class CrearEquipos extends javax.swing.JFrame {
                 .addGap(0, 0, 0)
                 .addComponent(lNumeroJugador, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addGroup(bgLayout.createSequentialGroup()
+                .addGap(410, 410, 410)
+                .addComponent(jLabel2)
+                .addGap(14, 14, 14)
+                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(bgLayout.createSequentialGroup()
+                .addGap(411, 411, 411)
+                .addComponent(jLabel6)
+                .addGap(24, 24, 24)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(bgLayout.createSequentialGroup()
+                .addGap(411, 411, 411)
+                .addComponent(jLabel5)
+                .addGap(25, 25, 25)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(bgLayout.createSequentialGroup()
+                .addGap(1, 1, 1)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(451, 451, 451)
+                .addGap(410, 410, 410)
                 .addComponent(botonCrearJugador))
         );
         bgLayout.setVerticalGroup(
@@ -137,22 +220,36 @@ public class CrearEquipos extends javax.swing.JFrame {
             .addGroup(bgLayout.createSequentialGroup()
                 .addGap(60, 60, 60)
                 .addComponent(jLabel3)
+                .addGap(18, 18, 18)
+                .addGroup(bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(lTipoEquipo))
+                .addGap(22, 22, 22)
+                .addGroup(bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel4)
+                    .addComponent(lNumeroJugador))
+                .addGap(27, 27, 27)
                 .addGroup(bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(bgLayout.createSequentialGroup()
-                        .addGap(18, 18, 18)
-                        .addGroup(bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(lTipoEquipo))
-                        .addGap(22, 22, 22)
-                        .addGroup(bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel4)
-                            .addComponent(lNumeroJugador))
-                        .addGap(327, 327, 327)
-                        .addComponent(botonCrearJugador))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, bgLayout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 468, Short.MAX_VALUE)
-                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap())))
+                        .addGap(2, 2, 2)
+                        .addComponent(jLabel2))
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(10, 10, 10)
+                .addGroup(bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel6)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(10, 10, 10)
+                .addGroup(bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel5)
+                    .addGroup(bgLayout.createSequentialGroup()
+                        .addGap(20, 20, 20)
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(52, 52, 52)
+                .addGroup(bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(bgLayout.createSequentialGroup()
+                        .addGap(48, 48, 48)
+                        .addComponent(botonCrearJugador))))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -206,12 +303,21 @@ public class CrearEquipos extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel PanelImagenes;
     private javax.swing.JPanel bg;
     private javax.swing.JButton botonCrearJugador;
+    private javax.swing.JButton botonPrevio;
+    private javax.swing.JButton botonSiguiente;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
     private javax.swing.JLabel lNumeroJugador;
     private javax.swing.JLabel lTipoEquipo;
     // End of variables declaration//GEN-END:variables
